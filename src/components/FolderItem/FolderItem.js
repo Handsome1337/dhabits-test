acad75b55634ff2dd4a599fac997bf20c58f4416
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-06 21:07:15 +0300

    Добавляет иконки к элементам списка

--- src/components/FolderItem/FolderItem.js
+++ src/components/FolderItem/FolderItem.js
@@ -1,11 +1,36 @@
 import {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileImage, faBookOpen, faFileArchive } from '@fortawesome/free-solid-svg-icons'
 import {fetchData} from '../../utils';
 
 function FolderItem({item, SubList}) {
   const [isActive, setIsActive] = useState(false);
   const [children, setChildren] = useState({isSet: false, list: null});
 
+  // если бы это был реальный проект, извлечение расширения надо было бы реализовать по-другому
+  // в данном случае не учитывается, что папка может содержать в названии точку
+  const fileExt = item.title.split('.').pop();
+  let marker;
+
+  if (['jpg', 'epub', 'zip'].includes(fileExt)) {
+    let icon;
+    if (fileExt === 'jpg') {
+      icon = faFileImage;
+    } else if (fileExt === 'epub') {
+      icon = faBookOpen;
+    } else {
+      icon = faFileArchive;
+    }
+    marker = <FontAwesomeIcon icon={icon} size="xs" />;
+  } else {
+    marker = '▸';
+  }
+
   const toggleActive = () => {
+    if (marker !== '▸') {
+      return;
+    }
+
     setIsActive((state) => !state);
 
     if (item.children && !children.isSet) {
@@ -27,9 +52,11 @@ function FolderItem({item, SubList}) {
           className={`marker${isActive ? ' marker--active' : ''} btn position-absolute p-0`}
           onClick={toggleActive}
         >
-          ▸
+          {marker}
         </button>
-        {item.title}
+        <span className="title">
+          {item.title}
+        </span>
         {childrenList}
       </li>
     </>
