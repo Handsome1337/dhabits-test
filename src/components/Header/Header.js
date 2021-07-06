315272a27a893e5c28d616fb6c0d4878fc147598
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-06 16:21:49 +0300

    Добавляет шапку

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-dark text-white fs-4 fw-bold p-3 ps-5">
+      Digital Habits. Entrance test
+    </header>
+  );
+}
+
+export default Header;
